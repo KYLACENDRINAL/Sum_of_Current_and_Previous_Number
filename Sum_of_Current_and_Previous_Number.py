1ea8dcf6c6085e09d95ab62a51e73bde90592aa8
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-20 22:01:05 +0800

    Done creating the loop

--- Sum_of_Current_and_Previous_Number.py
+++ Sum_of_Current_and_Previous_Number.py
@@ -1,5 +1,11 @@
 # Print the sum of the current and previous number
 
 # Pseudocode
-# Create the loop
-# Print/display the sum of the current and previous number
+
+print ("Printing current and previous number and their sum in a range (10)")
+previous_num=0
+
+# Create the loop from 1 to 10
+for i in range (1, 11):
+    sum=previous_num+i
+    
